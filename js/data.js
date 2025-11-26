956e38b7148270e2f3ba6c18ed8e15724141c75c
NicolasPalsa <s2401220@edu.bc.fi>
2025-11-26 12:40:24 +0200

    add form

---
+++ js/data.js
@@ -0,0 +1,105 @@
+const teams = {
+    "teams":[
+        {"ID":1,"team_image":"https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/lal.png&h=200&w=200",
+            "team_name":"Los Angeles Lakers",
+            "description":"The Los Angeles Lakers are one of the most influential teams in professional basketball, a historic franchise with a storied legacy and many Hall of Famers. Across decades, they’ve defined multiple eras—from George Mikan’s early dominance to the Showtime years with Magic Johnson and Kareem Abdul-Jabbar, to the three-peat power of Kobe Bryant and Shaquille O’Neal—and now the modern era led by LeBron James, who added another championship to the team’s legacy in 2020. Their identity is built on excellence, star talent, and constant championship expectations, supported by a global fanbase and a cultural presence that goes far beyond basketball. With iconic rivalries, especially against the Boston Celtics, and a long history of attracting generational superstars, the Lakers remain a central force in NBA history and a symbol of sustained ambition and success.",
+            "added_to_ball_of_fame":"2025-11-19T00:00:00.000Z",
+            "highlight_1": "https://www.youtube.com/embed/FVmO71_mxrk?si=0WDCA-ZOfyILm3Bi",
+            "highlight_2": "https://www.youtube.com/embed/_s2owBLdJ4Y?si=FOKOZJrbPVuncREn",
+            "highlight_3": "https://www.youtube.com/embed/M3Eki8m3EMY?si=1A0d5d-JK6M03FWo",
+            "players":[
+                {"player_image": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png", "player_name":"LeBron James","height":206,"best_ppg":27.1,"freethrow_percentage":"73.6%"},
+                {"player_image": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/110.png", "player_name":"Kobe Bryant","height":198,"best_ppg":25,"freethrow_percentage":"83.7%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/77142.png", "player_name":"Magic Johnson","height":206,"best_ppg":19.5,"freethrow_percentage":"84.8%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/76003.png", "player_name":"Kareem Abdul-Jabbar","height":218,"best_ppg":24.6,"freethrow_percentage":"72.1%"},
+                {"player_image": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/614.png", "player_name":"Shaquille O'Neal","height":216,"best_ppg":23.7,"freethrow_percentage":"52.7%"}
+            ]
+        },
+        {"ID":2,"team_image":"https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/gs.png",
+            "team_name":"Golden State Warriors",
+            "description":"The Golden State Warriors are a franchise known for its shooting, pace, and modern dynasty, redefining how basketball is played through elite spacing, relentless ball movement, and record-breaking three-point shooting. Their rise began with the emergence of Stephen Curry, whose shooting range restructured defensive strategy league-wide, supported by Klay Thompson’s sharpshooting and Draymond Green’s versatility. Under Steve Kerr, the Warriors built a system focused on speed, off-ball movement, and unselfish play, leading to multiple championships and one of the most dominant stretches in NBA history. Their impact goes far beyond wins: they altered the league’s offensive philosophy, influenced how teams are built, and cemented themselves as one of the defining teams of the modern era.",
+            "added_to_ball_of_fame":"2025-11-18T00:00:00.000Z",
+            "highlight_1": "https://www.youtube.com/embed/aAXSoKRrHXs?si=hhL_sUxGPyDgc7Bt",
+            "highlight_2": "https://www.youtube.com/embed/O8QuVSRbybY?si=FPxE8e0ignK9h6Eo",
+            "highlight_3": "https://www.youtube.com/embed/BucRbeyYSHw?si=2fYHh-1mi5VcrbMG",
+            "players":[
+                {"player_image": "https://a.espncdn.com/i/headshots/nba/players/full/4432171.png", "player_name":"Stephen Curry","height":188,"best_ppg":24.7,"freethrow_percentage":"90.9%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/76375.png", "player_name":"Wilt Chamberlain","height":216,"best_ppg":30.1,"freethrow_percentage":"51.1%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/600013.png", "player_name":"Rick Barry","height":198,"best_ppg":23.2,"freethrow_percentage":"89.3%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/76056.png", "player_name":"Paul Arizin","height":193,"best_ppg":22.8,"freethrow_percentage":"80.2%"},
+                {"player_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-qW3ZLiuKuObdIF1rn6xodNTzYAWKXpvpw&s", "player_name":"Chris Mullin","height":200,"best_ppg":18.2,"freethrow_percentage":"86.5%"}
+            ]
+        },
+        {"ID":3,"team_image":"https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/chi.png",
+            "team_name":"Chicago Bulls",
+            "description":"The Chicago Bulls are legendary for their 90s dominance and Michael Jordan’s era, a period that transformed the franchise into a global phenomenon and set a standard for excellence in professional basketball. With Jordan’s unmatched scoring and competitive drive, Scottie Pippen’s elite two-way play, and Phil Jackson’s triangle offense, the Bulls secured six championships in eight years and became the defining team of the decade. Their style, star power, and cultural impact extended far beyond the court, influencing fashion, media, and international interest in the NBA. Even after the dynasty years, the Bulls’ identity remains tied to that historic run, standing as one of the league’s most iconic and influential franchises.",
+            "added_to_ball_of_fame":"2025-11-17T00:00:00.000Z",
+            "highlight_1": "https://www.youtube.com/embed/oX6J8gDfzlg?si=c7iVyJ5cuh0BOmNY",
+            "highlight_2": "https://www.youtube.com/embed/SRW40pAqouI?si=K36XmYA4lw9ipkzO",
+            "highlight_3": "https://www.youtube.com/embed/VqlgO5_JWOA?si=ZDD4EcYFmm1LOTlE",
+            "players":[
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/893.png", "player_name":"Michael Jordan","height":198,"best_ppg":30.1,"freethrow_percentage":"83.5%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/937.png", "player_name":"Scottie Pippen","height":203,"best_ppg":16.1,"freethrow_percentage":"70.4%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/201565.png", "player_name":"Derrick Rose","height":191,"best_ppg":18.2,"freethrow_percentage":"83.0%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/23.png", "player_name":"Dennis Rodman","height":201,"best_ppg":7.3,"freethrow_percentage":"58.4%"},
+                {"player_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScGYdMy46nxLshuqHa8E-NxBZ0AwrTfEN98g&s", "player_name":"Horace Grant","height":206,"best_ppg":13.2,"freethrow_percentage":"70.8%"}
+            ]
+        },
+        {"ID":4,"team_image":"https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/2.png",
+            "team_name":"Boston Celtics",
+            "description":"The Boston Celtics are one of the most successful and tradition-rich franchises in basketball history, defined by a culture of winning, toughness, and consistency across generations. From the Bill Russell dynasty that established the team as a dominant force, to the Larry Bird era that fueled some of the NBA’s greatest rivalries, to the modern resurgence led by stars like Paul Pierce and later the Jayson Tatum–Jaylen Brown core, the Celtics have maintained a reputation for excellence built on defense, teamwork, and player development. Their unmatched collection of championships, iconic rivalries—especially with the Lakers—and deep historical influence make them a cornerstone of the NBA and a symbol of sustained competitive identity.",
+            "added_to_ball_of_fame":"2025-11-16T00:00:00.000Z",
+            "highlight_1": "https://www.youtube.com/embed/8E36LwXGADY?si=VLzTJYxTcoGgNAgY",
+            "highlight_2": "https://www.youtube.com/embed/bXJfRKL82oQ?si=Znlv4uMcUt1VmY3f",
+            "highlight_3": "https://www.youtube.com/embed/IgGS629f5mU?si=XEH6xwH225ZI6ZrK",
+            "players":[
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/1449.png", "player_name":"Larry Bird","height":206,"best_ppg":24.3,"freethrow_percentage":"88.6%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/78049.png", "player_name":"Bill Russell","height":208,"best_ppg":15.1,"freethrow_percentage":"56.1%"},
+                {"player_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY4YQ6Yd66Qk3VYGJN5rG5JIdXPrbK-j17NQ&s", "player_name":"Paul Pierce","height":201,"best_ppg":19.7,"freethrow_percentage":"80.6%"},
+                {"player_image": "https://cdn.nba.com/headshots/nba/latest/1040x760/1450.png", "player_name":"Kevin McHale","height":208,"best_ppg":17.9,"freethrow_percentage":"76.2%"},
+                {"player_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRU50yWLmt0Xf64eSq2NXfSMvf_1TQ-ab6Tg&s", "player_name":"Robert Parish","height":216,"best_ppg":14.5,"freethrow_percentage":"72.1%"}
+            ]
+        }
+    ]
+}
+
+document.getElementById('search').addEventListener('input', (e) => {
+    const value = document.getElementById('search').value
+    results = []
+    const div = document.getElementById('result')
+    div.innerHTML = ''
+
+    var searchArray = []
+    teams.teams.forEach(team => {
+        searchArray.push(team.team_name)
+        team.players.forEach(player => {
+            searchArray.push(player.player_name)
+        })
+    })
+
+    search(searchArray, value)
+
+    for (result in results) {
+        if (result >= 5) {
+            break
+        } else {
+            const p = document.createElement('p')
+            p.innerHTML = results[result]
+            p.id = results[result].toString().replaceAll(' ', '_')
+            div.append(p)
+        }
+    }
+})
+
+function search(searchList, searchTerm) {
+    if (!searchTerm) {
+        console.log('Nothing searched');
+    } else {
+        for (i of searchList) {
+            if (i.slice(0, searchTerm.length).toLowerCase() === searchTerm.toLowerCase()) {
+                results.push(i)
+            }
+        }
+        results.sort()
+    }
+}
